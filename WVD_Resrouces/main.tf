84811f76cf0c66b0d12941dabe34d21f4dde5765
Corey Callaway <coreylcallaway@gmail.com>
2021-04-01 12:11:29 -0400

    Add TF For WVD Resources Only

---
+++ WVD_Resrouces/main.tf
@@ -0,0 +1,83 @@
+# Get AzureRM Terraforn Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      # version = "2.31.1" Or Greater Required for WVD
+      version = "=2.46.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "default" {
+  name     = var.rgname
+  location = var.region
+}
+
+# Create WVD Host Pool that is a Pooled Type
+resource "azurerm_virtual_desktop_host_pool" "wvdpool01" {
+  location            = var.region
+  resource_group_name = azurerm_resource_group.default.name
+  name                     = var.pooledhpname
+  friendly_name            = var.pooledhpfriendlyname
+  description              = var.pooledhpdescription
+  type                     = "Pooled"
+  load_balancer_type       = "BreadthFirst" # Options: BreadthFirst / DepthFirst
+  maximum_sessions_allowed = 10
+
+  registration_info {
+    expiration_date = "2021-04-03T08:00:00Z" # Must be set to a time between 1 hour in the future & 27 days in the future
+  }
+}
+
+#Create Desktop Application Group (DAG) for wvdpool01
+resource "azurerm_virtual_desktop_application_group" "wvdpool01DAG" {
+  name                = var.pooledhpdesktopappname
+  location            = var.region
+  resource_group_name = azurerm_resource_group.default.name
+
+  type          = "Desktop"
+  host_pool_id  = azurerm_virtual_desktop_host_pool.wvdpool01.id
+  friendly_name = var.pooledhpdesktopappfriendlyname
+  description   = var.pooledhpdesktopappdescription
+}
+
+#Create RemoteApp Application Group
+resource "azurerm_virtual_desktop_application_group" "wvdpool01AppG" {
+  name                = var.pooledhpremoteappname
+  location            = var.region
+  resource_group_name = azurerm_resource_group.default.name
+
+  type          = "RemoteApp"
+  host_pool_id  = azurerm_virtual_desktop_host_pool.wvdpool01.id
+  friendly_name = var.pooledhpremoteappfriendlyname
+  description   = var.pooledhpremoteappdescription
+}
+
+# Create Workspace
+resource "azurerm_virtual_desktop_workspace" "workspace" {
+  name                = var.workspace
+  location            = var.region
+  resource_group_name = azurerm_resource_group.default.name
+
+  friendly_name = var.workspacefriendlyname
+  description   = var.workspacedesc
+}
+
+# Associate RemoteApp Application Group with Workspace
+resource "azurerm_virtual_desktop_workspace_application_group_association" "workspaceremoteapp" {
+  workspace_id         = azurerm_virtual_desktop_workspace.workspace.id
+  application_group_id = azurerm_virtual_desktop_application_group.wvdpool01AppG.id
+
+}
+
+# Associate Desktop Application Group with Workspace
+resource "azurerm_virtual_desktop_workspace_application_group_association" "workspacedesktop" {
+  workspace_id         = azurerm_virtual_desktop_workspace.workspace.id
+  application_group_id = azurerm_virtual_desktop_application_group.wvdpool01DAG.id
+
+}
---
+++ WVD_Resrouces/variables.tf
@@ -0,0 +1,69 @@
+variable "rgname" {
+  description = "Resource Group Name"
+  default     = "TF-WVD-RG"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "East US"
+}
+
+variable "pooledhpname" {
+  description = "Pooled Host Pool Name"
+  default     = "HP01-Pooled"
+}
+
+variable "pooledhpfriendlyname" {
+  description = "Pooled Host Pool Friendly Name"
+  default     = "HP01 Pooled Host Pool"
+}
+
+variable "pooledhpdescription" {
+  description = "Pooled Host Pool Description"
+  default     = "A Pooled Host Pool"
+}
+
+variable "pooledhpdesktopappname" {
+  description = "Pooled Host Pool Desktop App Group Friendly Name"
+  default     = "HP01-Pooled-DAG"
+}
+
+variable "pooledhpdesktopappfriendlyname" {
+  description = "Pooled Host Pool Desktop App Group Friendly Name"
+  default     = "HP01 Desktop Application Group"
+}
+
+variable "pooledhpdesktopappdescription" {
+  description = "Pooled Host Pool Desktop App Group Description"
+  default     = "A Desktop Application Group for HP01"
+}
+
+variable "pooledhpremoteappname" {
+  description = "Pooled Host Pool RemoteApp App Group Name"
+  default     = "HP01-Pooled-AppGroup"
+}
+
+variable "pooledhpremoteappfriendlyname" {
+  description = "Pooled Host Pool RemoteApp App Group Friendly Name"
+  default     = "HP01 Remote Application Group"
+}
+
+variable "pooledhpremoteappdescription" {
+  description = "Pooled Host Pool RemoteApp App Group Description"
+  default     = "A Remote Application Group for HP01"
+}
+
+variable "workspace" {
+  description = "WVD Workspace Name"
+  default     = "HP01-WS-TF"
+}
+
+variable "workspacefriendlyname" {
+  description = "WVD Workspace Friendly Name"
+  default     = "HP01 Workspace"
+}
+
+variable "workspacedesc" {
+  description = "WVD Workspace Description"
+  default     = "A Workspace for HP01"
+}
