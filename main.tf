4274758f8f8d704af51f77796e106c1c0cede4ba
Corey Callaway <coreylcallaway@gmail.com>
2021-06-11 13:08:25 -0400

    Update DSC URI to latest

--- main.tf
+++ main.tf
@@ -189,7 +189,7 @@ resource "azurerm_virtual_machine_extension" "vmext_dsc" {
   
   settings = <<-SETTINGS
     {
-      "modulesUrl": "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration.zip",
+      "modulesUrl": "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_3-10-2021.zip",
       "configurationFunction": "Configuration.ps1\\AddSessionHost",
       "properties": {
         "hostPoolName": "${var.pooledhpname}",
